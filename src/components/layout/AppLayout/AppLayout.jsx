86ad4c4435d2b703dfc8f1cd17f052afe1ce81a5
Bardia Asadi <bardiaasd7@gmail.com>
2026-09-10 20:12:34 +0330

    fix: reset scroll position on route change

--- src/components/layout/AppLayout/AppLayout.jsx
+++ src/components/layout/AppLayout/AppLayout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router";
 import Header from "../Header";
 import Footer from "../Footer";
 import { Toaster } from "@/components/ui/toaster";
+import ScrollToTop from "./ScorllToTop";
 
 const AppLayout = () => {
     return (
         <div className="min-h-dvh bg-background text-foreground flex flex-col">
+            <ScrollToTop />
             <Header />
             <main className="flex-1">
                 <Outlet />
